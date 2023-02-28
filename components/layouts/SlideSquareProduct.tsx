eba4dfbc98f5059ec082f798001239f4e2f062dd
minkyeong <mgk04222@naver.com>
2023-02-28 18:11:41 +0900

    SlideSquareProduct Component

---
+++ components/layouts/SlideSquareProduct.tsx
@@ -0,0 +1,21 @@
+import { productCardData } from "@/types/type";
+import ProductCard from "../ui/ProductCard";
+
+export default function SlideSquareProduct(props : {title:string, itemData:productCardData[]}) {
+  return (
+    <section className="slide-square-product-lists">
+      <div>
+        <p className="slide-square-product__main-title">{props.title}</p>
+        <div className="slide-square-product-list">
+            {
+                props.itemData.map( item => (
+                    <ProductCard 
+                        data = {item}
+                    />
+                ))
+            }
+        </div>
+      </div>
+    </section>
+  );
+}
