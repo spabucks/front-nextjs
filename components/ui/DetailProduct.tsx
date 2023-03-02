2b21880fe32c7aedb5853e29b702a50a8befa2aa
minkyeong <mgk04222@naver.com>
2023-03-02 19:24:53 +0900

    product detail component

---
+++ components/ui/DetailProduct.tsx
@@ -0,0 +1,59 @@
+import { recommandData } from '@/types/type'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import { detailProduct } from '@/types/type'
+
+export default function DetailProduct(props:{data:detailProduct}) {
+    const [data, setData] = useState<detailProduct>()
+    const { query } = useRouter();
+
+    console.log(query.productId)
+    useEffect(() => {
+        {/*axios.get(`http://localhost:3001/data/${query.productId}`) */}
+        axios.get(`http://10.10.10.173:8081/api/v1/product/get/${query.productId}`)
+        
+        .then( res => {
+          console.log(res.data)
+          setData(res.data)
+        })
+        .catch( err  => console.log( err ))
+      },[query.productId])
+
+  return (
+    <>
+      <section className="section-main">
+        <div className="product-main-info">
+          <img
+            src={data?.imgUrl}
+            alt="main-img"
+          />
+          <div className="product-detail-main__info">
+            <div className="product-detail-main__title">
+              <h2>{data?.title}</h2>
+              <img src="../assets/images/icons/share.svg" />
+            </div>
+            <p>
+              {data?.description}
+            </p>
+            <p className="product-charge">
+              <span>{data?.price}</span>원
+            </p>
+          </div>
+        </div>
+        <div className="sep"></div>
+        <div className="product-detail-info">
+          <h3>상품정보</h3>
+          {data?.productDetailImgUrl.map((item)=>{
+          return(
+            <img
+            src={item}
+            alt="상품상세정보"
+            />  
+          )
+          })}
+        </div>
+      </section>
+    </>
+  );
+}
