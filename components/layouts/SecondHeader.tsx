b23e92c98046510246d8e3b49b4d48548c99abd1
minkyeong <mgk04222@naver.com>
2023-03-02 19:23:57 +0900

    secondheader add

---
+++ components/layouts/SecondHeader.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { headerMenus, headerRightIcons } from "@/data/navMenuDatas";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+export default function SecondHeader() {
+  return (
+    <header>
+    <div className="sub-header">
+      <img src="../assets/images/icons/menu.svg" alt="메뉴아이콘" />
+      <p className="sub-header-title">온라인 스토어</p>
+      <img src="../assets/images/icons/close.svg" alt="닫기아이콘"/>
+    </div>
+  </header>
+  );
+}
