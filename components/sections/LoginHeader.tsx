31e9c76500752127021071f65ad78d4d42110eb0
minkyeong <mgk04222@naver.com>
2023-03-06 14:08:54 +0900

    LoginHeader component

---
+++ components/sections/LoginHeader.tsx
@@ -0,0 +1,12 @@
+
+export default function LoginHeader() {
+  return (
+    <header>
+    <div className="login-header">
+      <img src="../assets/images/icons/left-chevron.svg" alt="메뉴아이콘" />
+      <p className="login-header-title">로그인</p>
+      <img src="../assets/images/icons/close.svg" alt="닫기아이콘"/>
+    </div>
+  </header>
+  );
+}
