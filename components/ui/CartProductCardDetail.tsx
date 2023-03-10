bd23d4004499cc48475667b366619ea623447ae6
minkyeong <mgk04222@naver.com>
2023-03-10 15:25:08 +0900

    cartproductcarddetail component

---
+++ components/ui/CartProductCardDetail.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+export interface ChildProps {
+    isClick?: Boolean,
+    setIsClick?: React.Dispatch<React.SetStateAction<Boolean>>,
+    data: {title:string, price:number}
+  }
+
+export default function CartProductCardDetail({data, isClick, setIsClick}:ChildProps) {
+  const [count, setCount]=useState(1)
+    const handleView = () => {
+        setIsClick && setIsClick(!isClick)
+    }
+
+  return (
+    <div className="buy-product-lists border-top">
+        
+        <div className='view-btn' onClick={handleView}></div>
+        {
+            isClick ? 
+            <>
+            <div className="buy-product-list">
+            <p>{data.title}</p>
+            <div className="buy-product-count-charge">
+                <div className="buy-product-count">
+                {count>0? <button onClick={() => {setCount(count - 1)}}>-</button> : <button>-</button> }
+                <p>{count}</p>
+                {count<5? <button onClick={() => {setCount(count + 1)}}>+</button> : <button>+</button> }
+                </div>
+                <div className="buy-product-charge">{data.price*count}원</div>
+                {/* <div className='buy-announcement-none'>해당 상품은 회원당 최대 5개까지 구매 가능합니다.</div> */}
+            </div>
+            </div>
+            {/* <div className='buy-announcement-none'>해당 상품은 회원당 최대 5개까지 구매 가능합니다.</div> */}
+            <div className="buy-product-total">
+            <p>합계 {data.price*count}원</p>
+            </div>
+            </>
+            :
+            ""
+        }
+        
+      </div>
+  )
+}
