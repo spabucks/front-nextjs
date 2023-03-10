e20daf7568426f75f9a79917932a03be200dbfa7
minkyeong <mgk04222@naver.com>
2023-03-10 15:26:31 +0900

    cartproductcheck component

---
+++ components/ui/CartProductCheck.tsx
@@ -0,0 +1,13 @@
+export default function CartProductCheck() {
+  return (
+    <>
+      <div className="section-cart-middle border-top">
+        <div>
+          <p>상품 1건 23,000원 배송비 3,000원 = 총 26,000원</p>
+          <p>7,000원 더 담으면 무료배송</p>
+        </div>
+        <button type="button">더 담으러 가기</button>
+      </div>
+    </>
+  );
+}
