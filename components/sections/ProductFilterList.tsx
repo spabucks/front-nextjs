caf2df56617d1737c662c1cde7c610412a527cd7
minkyeong <mgk04222@naver.com>
2023-03-23 21:34:22 +0900

    productfilterList component

--- components/sections/ProductFilterList.tsx
+++ components/sections/ProductFilterList.tsx
@@ -1,19 +1,53 @@
-import React from 'react'
-import {filterProductList } from '@/types/type';
-import ProductFilterListCard from './ProductFilterListCard';
+import React from "react";
+import { filterProductList } from "@/types/type";
+import ProductFilterListCard from "./ProductFilterListCard";
+import { useState } from "react";
+import FilterHeader from "../pages/filter/FilterHeader";
 
-
-export default function ProfuctFilterList(props:{
-    itemData: filterProductList[];
+export default function ProfuctFilterList(props: {
+  itemData: filterProductList[];
 }) {
+  const [product, setProduct] = useState<filterProductList[]>(props.itemData);
+
+  console.log("product", product);
   return (
-    <section className="filter-product-lists">
-    <div className="best-product-list">
-      {props.itemData &&
-        props.itemData.map((items, i) => (
-          <ProductFilterListCard key={items.id} data={items} />
-        ))}
-    </div>
-  </section>
-  )
+    <>
+      <div className="order-product-btn">
+        <button
+          className="order-high-product"
+          onClick={() => {
+            let highcopy = [...props.itemData];
+            highcopy.sort((a, b) => b.price - a.price);
+            console.log("highcopy", highcopy);
+            setProduct(highcopy);
+          }}
+        >
+          높은가격순
+        </button>
+        <button
+          className="order-low-product"
+          onClick={() => {
+            let lowcopy = [...props.itemData];
+            lowcopy.sort((a, b) => a.price - b.price);
+            console.log("lowcopy", lowcopy);
+            setProduct(lowcopy);
+          }}
+        >
+          낮은가격순
+        </button>
+      </div>
+
+      <section className="filter-product-lists">
+        <div className="best-product-list">
+          {props.itemData &&
+            props.itemData.map((items, i) => (
+              <ProductFilterListCard key={items.id} data={items} />
+            ))}
+        </div>
+        {props.itemData.length === 0 && (
+          <div className="filter-nonproduct">조회되는 상품이 없습니다.</div>
+        )}
+      </section>
+    </>
+  );
 }
