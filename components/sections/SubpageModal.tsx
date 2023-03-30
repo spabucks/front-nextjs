4ff05fd35aba0fc6c936bf4c5e8f12cac3094a1f
minkyeong <mgk04222@naver.com>
2023-03-30 16:18:49 +0900

    로그인 유지

--- components/sections/SubpageModal.tsx
+++ components/sections/SubpageModal.tsx
@@ -7,40 +7,54 @@ import Head from "next/head";
 import { useRecoilState } from "recoil";
 import { subPage } from "@/state/subPage";
 import { userState } from "@/state/userState";
+
+import { useCookies } from "react-cookie";
 export default function SubpageModal() {
-    const [data, setData] = useState<subPageMenu[]>([]);
-    const [subPageModal,setSubpageModal] = useRecoilState(subPage);
-    const [loginData,setLoginData]=useRecoilState(userState)
-    const handleSubpageClose = () =>{
-        setSubpageModal(false)
+  const [isLogin, setIsLogin] = useRecoilState(userState);
+  const [cookies, setCookie] = useCookies(["id"]);
+  const [subPageModal, setSubpageModal] = useRecoilState(subPage);
+  const [loginData, setLoginData] = useRecoilState(userState);
+  useEffect(() => {
+    const myLogin = cookies.id;
+    if (myLogin && !isLogin.isLogin) {
+      console.log("로그인 되어있음");
+      setIsLogin({
+        userId: localStorage.getItem("userId") || "",
+        accessToken: localStorage.getItem("accessToken") || "",
+        isLogin: true,
+      });
+      // setIsLogin({
+      //   userId: localStorage.getItem("userId") || "",
+      //   accessToken: localStorage.getItem("accessToken") || "",
+      //   refreshToken: localStorage.getItem("refreshToken") || "",
+      //   isLogin: true,
+      // });
     }
-    useEffect(() => {
-      axios
-        .get(`http://localhost:3001/subpagemanu`)
-        .then((res) => {
-          setData(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }, []);
+  },[]);
+  const handleSubpageClose = () => {
+    setSubpageModal(false);
+  };
+  if (!subPageModal) {
+    return null;
+  }
 
-    if(!subPageModal) {
-        return null;
-    }
-    return (
-    
-      <div className="modal">
-        <div className="sub-page__main-header sub-boder-under">
-          <div className="sub-page__main-header-content">
-            <div className="sub-page__main-header-content-icon">
-                <img src="assets/images/icons/close.svg" onClick={handleSubpageClose}/>
-            </div>
+  return (
+    <div className="modal">
+      <div className="sub-page__main-header sub-boder-under">
+        <div className="sub-page__main-header-content">
+          <div className="sub-page__main-header-content-icon">
+            <img
+              src="assets/images/icons/close.svg"
+              onClick={handleSubpageClose}
+            />
+          </div>
 
-            {loginData.isLogin === true ? (
+          {loginData.isLogin === true ? (
             <>
               <h2>Welcome</h2>
-              <p className="welcome-title">온라인 스토어에 오신 것을 환영합니다.</p>
+              <p className="welcome-title">
+                온라인 스토어에 오신 것을 환영합니다.
+              </p>
             </>
           ) : (
             <>
@@ -55,103 +69,114 @@ export default function SubpageModal() {
               </p>
             </>
           )}
-          </div>
         </div>
-        <section className="sub-page__main-contents">
-          <div className="sub-page__main-contents-title">
+      </div>
+      <section className="sub-page__main-contents">
+        <div className="sub-page__main-contents-title">
+          <Link
+            href={`http://localhost:3000/filter?bigCategory=0`}
+            className="sub-page__main-contents-a"
+            onClick={handleSubpageClose}
+          >
+            전체상품보기
+            <img
+              className="sub-change-left-icon"
+              src="assets/images/icons/left-chevron.svg"
+            />
+          </Link>
+        </div>
+        <div className="sub-page__main-first-content">
+          <div className="sub-page__main-content-category">
             <Link
-              href={`http://localhost:3000/filter?bigCategory=0`}
-              className="sub-page__main-contents-a"
+              href="http://localhost:3000/filter?bigCategory=1"
               onClick={handleSubpageClose}
             >
-              전체상품보기
-              <img
-                className="sub-change-left-icon"
-                src="assets/images/icons/left-chevron.svg"
-              />
+              <img src="assets/images/products/케이크.jpg" />
+              <p>케이크</p>
             </Link>
           </div>
-          <div className="sub-page__main-first-content">
-            <div className="sub-page__main-content-category">
-              <Link href="http://localhost:3000/filter?bigCategory=1"
-              onClick={handleSubpageClose}>
-                <img src="assets/images/products/케이크.jpg" />
-                <p>케이크</p>
-              </Link>
-            </div>
-            <div className="sub-page__main-content-category">
-              <Link href="http://localhost:3000/filter?bigCategory=2"
-              onClick={handleSubpageClose}>
-                <img src="assets/images/products/텀블러보온병.jpg" />
-                <p>텀블러/보온병</p>
-              </Link>
-            </div>
-            <div className="sub-page__main-content-category">
-              <Link href="http://localhost:3000/filter?bigCategory=3"
-              onClick={handleSubpageClose}>
-                <img src="assets/images/products/머그컵.jpg" />
-                <p>머그/컵</p>
-              </Link>
-            </div>
+          <div className="sub-page__main-content-category">
+            <Link
+              href="http://localhost:3000/filter?bigCategory=2"
+              onClick={handleSubpageClose}
+            >
+              <img src="assets/images/products/텀블러보온병.jpg" />
+              <p>텀블러/보온병</p>
+            </Link>
           </div>
-          <div className="sub-page__main-second-content">
-            <div className="sub-page__main-content-category">
-              <Link href="http://localhost:3000/filter?bigCategory=4"
-              onClick={handleSubpageClose}>
-                <img src="assets/images/products/라이프스타일.jpg" />
-                <p>라이프스타일</p>
-              </Link>
-            </div>
-            <div className="sub-page__main-content-category">
-              <Link href="http://localhost:3000/filter?bigCategory=5"
-              onClick={handleSubpageClose}>
-                <img src="assets/images/products/티커피용품.jpg" />
-                <p>티/커피용품</p>
-              </Link>
-            </div>
-            <div className="sub-page__main-content-category">
-              <Link href="http://localhost:3000/filter?bigCategory=6"
-              onClick={handleSubpageClose}>
-                <img src="assets/images/products/세트.jpg" />
-                <p>세트</p>
-              </Link>
-            </div>
+          <div className="sub-page__main-content-category">
+            <Link
+              href="http://localhost:3000/filter?bigCategory=3"
+              onClick={handleSubpageClose}
+            >
+              <img src="assets/images/products/머그컵.jpg" />
+              <p>머그/컵</p>
+            </Link>
           </div>
-        </section>
-  
-        <section className="sub-page__sub-contents">
-        <div className="sub-page__sub-content boder-under">
-        <div className="sub-page__sub-content-title">
-          <p>기획전</p>
-          <p>실행중인 기획을 만나보세요</p>
         </div>
-        <Link href="/event?category=1" onClick={handleSubpageClose}>
+        <div className="sub-page__main-second-content">
+          <div className="sub-page__main-content-category">
+            <Link
+              href="http://localhost:3000/filter?bigCategory=4"
+              onClick={handleSubpageClose}
+            >
+              <img src="assets/images/products/라이프스타일.jpg" />
+              <p>라이프스타일</p>
+            </Link>
+          </div>
+          <div className="sub-page__main-content-category">
+            <Link
+              href="http://localhost:3000/filter?bigCategory=5"
+              onClick={handleSubpageClose}
+            >
+              <img src="assets/images/products/티커피용품.jpg" />
+              <p>티/커피용품</p>
+            </Link>
+          </div>
+          <div className="sub-page__main-content-category">
+            <Link
+              href="http://localhost:3000/filter?bigCategory=6"
+              onClick={handleSubpageClose}
+            >
+              <img src="assets/images/products/세트.jpg" />
+              <p>세트</p>
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <section className="sub-page__sub-contents">
+        <div className="sub-page__sub-content boder-under">
+          <div className="sub-page__sub-content-title">
+            <p>기획전</p>
+            <p>실행중인 기획을 만나보세요</p>
+          </div>
+          <Link href="/event?category=1" onClick={handleSubpageClose}>
             <div className="sub-page__sub-content-icon">
               <img
                 className="sub-change-left-icon"
                 src="assets/images/icons/left-chevron.svg"
               />
             </div>
-            </Link>
-      </div>
-        </section>
-        <section className="sub-page__sub-contents">
-        <div className="sub-page__sub-content boder-under">
-        <div className="sub-page__sub-content-title">
-          <p>베스트</p>
-          <p>스타벅스의 베스트 상품을 만나보세요</p>
+          </Link>
         </div>
-        <Link href="/best?category=1" onClick={handleSubpageClose}>
+      </section>
+      <section className="sub-page__sub-contents">
+        <div className="sub-page__sub-content boder-under">
+          <div className="sub-page__sub-content-title">
+            <p>베스트</p>
+            <p>스타벅스의 베스트 상품을 만나보세요</p>
+          </div>
+          <Link href="/best?category=1" onClick={handleSubpageClose}>
             <div className="sub-page__sub-content-icon">
               <img
                 className="sub-change-left-icon"
                 src="assets/images/icons/left-chevron.svg"
               />
             </div>
-            </Link>
-      </div>
-        </section>
-      </div>
-    );
-  }
-  
+          </Link>
+        </div>
+      </section>
+    </div>
+  );
+}
