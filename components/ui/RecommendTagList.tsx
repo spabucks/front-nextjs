a6a6549256d65e89ba460beda81b566a7181bdfe
minkyeong <mgk04222@naver.com>
2023-04-01 15:12:19 +0900

    recommandtaglist component

---
+++ components/ui/RecommendTagList.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function RecommendTagList() {
+  return (
+    <>
+     <section>
+          <div className="section-search-tag">
+            <h3>추천태그</h3>
+            <div className="section-search-tags">
+              <div className="search-item">#케이크</div>
+              <div className="search-item">#리유저블</div>
+              <div className="search-item">#도트머그</div>
+            </div>
+            <div className="section-search-tags">
+              <div className="search-item">#리드</div>
+              <div className="search-item">#그린코어</div>
+              <div className="search-item">#핸디 데스크</div>
+            </div>
+          </div>
+        </section>
+    </>
+  )
+}
