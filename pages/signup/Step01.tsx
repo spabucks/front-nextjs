eb7fa76b18f09002ce10733f5b14fa00e7af9ec2
minkyeong <mgk04222@naver.com>
2023-04-01 19:43:06 +0900

    회원가입 1단계 동의 수정

--- pages/signup/Step01.tsx
+++ pages/signup/Step01.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { inputRegisterType } from "@/types/UserRequest/Request";
 import { privateAgreeType } from "@/types/UserRequest/Request";
 import CheckBox from "@/components/pages/signup/ui/CheckBox";
@@ -10,13 +10,14 @@ interface ChildProps {
 }
 
 export default function Step01({ inputData, setInputData }: ChildProps) {
-
   const [isAllAgree, setIsAllAgree] = useState<boolean>(false);
 
-  const [ agreeArray, setAgreeArray ] = useState<privateAgreeType>({} as privateAgreeType);
+  const [agreeArray, setAgreeArray] = useState<privateAgreeType>(
+    {} as privateAgreeType
+  );
 
-  useEffect(()=> {
-    setInputData({...inputData, privateAgree: agreeArray})
+  useEffect(() => {
+    setInputData({ ...inputData, privateAgree: agreeArray });
     if (
       agreeArray.isAgree &&
       agreeArray.isUseConfirm &&
@@ -26,84 +27,78 @@ export default function Step01({ inputData, setInputData }: ChildProps) {
     } else {
       setIsAllAgree(false);
     }
-  },[agreeArray])
+  }, [agreeArray]);
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name , checked } = e.target;
-    if(name === 'isAllAgree') {
+    const { name, checked } = e.target;
+    if (name === "isAllAgree") {
       setAgreeArray({
         isAgree: checked,
         isUseConfirm: checked,
         isAdvertisingConfirm: checked,
-    })} 
-    else {
+      });
+    } else {
       setAgreeArray({
         ...agreeArray,
         [name]: checked,
       });
     }
-  }
+  };
 
   return (
-    
-      
+    <div className="main-membership-section">
+      <div className="main-membership-img">
+        <img src="assets/images/logo/logo.png" alt="" />
+        <h1>
+          고객님 <br />
+          환영합니다!
+        </h1>
+      </div>
 
-      <div className="main-membership-section">
-        <div className="main-membership-img">
-          <img src="assets/images/logo/logo.png" alt="" />
-          <h1>
-            고객님 <br />
-            환영합니다!
-          </h1>
-        </div>
-
-        <div className="agree-check-form">
-          <div className="m-check">
-          <CheckBox 
-            lableText = '약관 전체동의'
-            isArrow = {false}
-            inputName = 'isAllAgree'
-            link = '/best_cake'
-            handler = { handleInput }
+      <div className="agree-check-form">
+        <div className="m-check">
+          <CheckBox
+            lableText="약관 전체동의"
+            isArrow={false}
+            inputName="isAllAgree"
+            link="/best_cake"
+            handler={handleInput}
             value={isAllAgree}
           />
-          <Separator 
-            color = '#e5e5e5'
-            gutter = {1}
+          <Separator color="#e5e5e5" gutter={1} />
+          <CheckBox
+            lableText="이용약관 동의(필수)"
+            isArrow={true}
+            inputName="isAgree"
+            link="/best_cake"
+            handler={handleInput}
+            value={agreeArray.isAgree}
           />
-          <CheckBox 
-            lableText = '이용약관 동의(필수)'
-            isArrow = {true}
-            inputName = 'isAgree'
-            link = '/best_cake'
-            handler = { handleInput }
-            value = { agreeArray.isAgree }
+          <CheckBox
+            lableText="개인정보 수집 및 이용동의(필수)"
+            isArrow={true}
+            inputName="isUseConfirm"
+            link="/best_cake"
+            handler={handleInput}
+            value={agreeArray.isUseConfirm}
           />
-          <CheckBox 
-            lableText = '개인정보 수집 및 이용동의(필수)'
-            isArrow = {true}
-            inputName = 'isUseConfirm'
-            link = '/best_cake'
-            handler = { handleInput }
-            value = { agreeArray.isUseConfirm }
-          />
-          <CheckBox 
-            lableText = '광고성 정보 수신동의(선택)'
-            isArrow = {true}
-            inputName = 'isAdvertisingConfirm'
-            link = '/best_cake'
-            handler = { handleInput }
-            value = { agreeArray.isAdvertisingConfirm }
+          <CheckBox
+            lableText="광고성 정보 수신동의(선택)"
+            isArrow={true}
+            inputName="isAdvertisingConfirm"
+            link="/best_cake"
+            handler={handleInput}
+            value={agreeArray.isAdvertisingConfirm}
           />
 
-            <div className="m-choice">
-              광고성 정보 수신 방법(선택)
-              <br />
-              <input type="checkbox" value="agree" /> <label>E-mail</label>
-              <input type="checkbox" value="agree" /> <label>SMS</label>
-            </div>
+          <div className="m-choice">
+            광고성 정보 수신 방법(선택)
+            <br />
+            <input type="checkbox" value="agree" /> <label>E-mail</label>
+            <input type="checkbox" value="agree" /> <label>SMS</label>
           </div>
         </div>
       </div>
+    </div>
   );
 }
