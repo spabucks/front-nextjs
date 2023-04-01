eb8cfe68f138f08bc549084ea65fa4f3963233ca
minkyeong <mgk04222@naver.com>
2023-04-01 13:40:22 +0900

    leftarrow component

---
+++ components/ui/Leftarrow.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+export default function Leftarrow() {
+  return (
+    <>
+      <Image
+        className="sub-change-left-icon"
+        src="assets/images/icons/left-chevron.svg"
+        alt="왼쪽화살표버튼"
+        height={50}
+        width={50}
+      ></Image>
+    </>
+  );
+}
