ff78061bf2ef8fb5bd8d141dfc7b3f9da1f25be8
minkyeong <mgk04222@naver.com>
2023-04-01 15:11:40 +0900

    searchnotword component

---
+++ components/ui/SearchNotWord.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function SearchNotWord() {
+  return (
+    <>
+      <section className="section-nosearch">
+        <div className="section-search-item">
+          <h3>최근 검색어가 없습니다.</h3>
+        </div>
+      </section>
+    </>
+  );
+}
