3adc504b11584e66fb0031bda2526ecee7752f35
minkyeong <mgk04222@naver.com>
2023-04-02 14:46:16 +0900

    mypage 구조 변경

--- components/sections/MyPageMenuLists.tsx
+++ components/sections/MyPageMenuLists.tsx
@@ -10,18 +10,17 @@ export default function MyPageMenuLists(props:{title:string}){
             {
               props.title==='서비스'?<>
               <MyPageMenuList menuicon={'assets/images/icons/online-order.svg'} title={'주문내역'}/>
-              <MyPageMenuList menuicon={'assets/images/icons/gift.svg'} title={'선물함'}/>
-              <MyPageMenuList menuicon={'assets/images/icons/coupon.svg'} title={'쿠폰'}/>
+              <MyPageMenuList menuicon={'assets/images/icons/gift.svg'} title={'장바구니'}/>
               <MyPageMenuList menuicon={'assets/images/icons/delivery-truck.svg'} title={'배송지 관리'}/>
-              <MyPageMenuList menuicon={'assets/images/icons/bell.svg'} title={'입고 알림 내역'}/>
               </>:
               <>
               <MyPageMenuList menuicon={'assets/images/icons/agree.svg'} title={'배송지 정보 수집 및 이용 동의'}/>
+              <div className="main-page-service-logout">로그아웃</div>
               </>
             }
-
           </div>
         </section>
+
         </>
     )
 }
