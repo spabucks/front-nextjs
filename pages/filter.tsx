06e7ce0d37b5f32ec47a1c0ce9275c9f8788ce9c
minkyeong <mgk04222@naver.com>
2023-04-09 22:08:34 +0900

    [Modifiy] filter page명 수정완료

---
+++ pages/filter.tsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+import { useRouter } from "next/router";
+
+import { filterProductList } from "@/types/type";
+
+import ProfuctFilterList from "@/components/pages/filter/ProductFilterList";
+import FirstHeader from "@/components/sections/FirstHeader";
+import FilterHeader from "@/components/pages/filter/FilterHeader";
+import Loading from "@/components/ui/Loading";
+import TopScrollBtn from "@/components/ui/TopScrollBtn";
+export default function Filter() {
+  const { asPath } = useRouter();
+  const [productList, setProductList] = useState<filterProductList[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const BaseUrl = process.env.baseApiUrl;
+    axios
+      .get(`${BaseUrl}/api/v1/product/search2?${asPath.split("?")[1]}`)
+      .then((res) => {
+        setProductList(res.data.data);
+        setIsLoading(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [asPath]);
+
+  return (
+    <>
+      <TopScrollBtn />
+      <FirstHeader />
+      <FilterHeader />
+      {!isLoading && (
+        <div style={{ height: "100vh" }}>
+          <Loading />
+        </div>
+      )}
+      {isLoading && <ProfuctFilterList itemData={productList} />}
+    </>
+  );
+}
